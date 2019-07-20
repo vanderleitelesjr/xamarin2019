2f0c29053b5b320eeee0b11ba27a6d5c077446b6
vande <vanderleitelesjr@gmail.com>
2019-07-20 09:34:49 -0300

    funcionando

---
+++ src/XamarinUP2018/XamarinUP2018/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinUP2018.ViewModels;
+using XamarinUP2018.Views;
+
+namespace XamarinUP2018
+{
+    public partial class App : PrismApplication
+    {
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(HomePage)}");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>(nameof(NavigationPage));
+            containerRegistry.RegisterForNavigation<HomePage, HomeViewModel>(nameof(HomePage));
+        }
+        
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ src/XamarinUP2018/XamarinUP2018/ViewModels/HomeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace XamarinUP2018.ViewModels
+{
+    public sealed class HomeViewModel : BindableBase
+    {
+        private string text;
+        private readonly PageDialogService pageDialogService;
+
+        public string Text {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+
+        public ICommand ShowAlert { get; }
+
+        public HomeViewModel(PageDialogService pageDialogService)
+        {
+            this.pageDialogService = pageDialogService;
+
+            ShowAlert = new DelegateCommand(async () => await ExecuteShowAllert());
+        }
+
+        private Task ExecuteShowAllert() 
+            => pageDialogService.DisplayAlertAsync("Hello", "The Message", "Ok");
+
+    }
+}
